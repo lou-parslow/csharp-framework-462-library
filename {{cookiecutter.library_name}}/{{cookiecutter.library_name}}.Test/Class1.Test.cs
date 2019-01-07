2b71942ad2ef9778698c4c814880e1f5fed15d3e
lou-parslow <lou.parslow@gmail.com>
2019-01-07 14:48:01 -0700

    initial add of files

---
+++ {{cookiecutter.library_name}}/{{cookiecutter.library_name}}.Test/Class1.Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace {{cookiecutter.library_name}}.Test
+{
+	[TestFixture]
+    public class Class1Test
+    {
+		[Test]
+		public void Greeting()
+		{
+			Assert.AreEqual("Hello, World!", Class1.Greeting);
+		}
+    }
+}
---
+++ {{cookiecutter.library_name}}/{{cookiecutter.library_name}}/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace {{cookiecutter.library_name}}
+{
+    public static class Class1
+    {
+		public static string Greeting { get { return "Hello, World!"; } }
+    }
+}
